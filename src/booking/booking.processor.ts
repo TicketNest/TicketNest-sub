ba581cfcbfd7d7ba576c1cd3a514f8c543c4f111
Shin Sungyun <sinsungyun1630@gmail.com>
2023-09-04 08:20:51 +0900

    Fix : stream 데이터 유실 시 무한 반복 에러 수정

--- src/booking/booking.processor.ts
+++ src/booking/booking.processor.ts
@@ -1,19 +1,21 @@
-import { BookingService } from './booking.service';
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
+//! Stream 에선 아래의 코드가 필요없음
 
-// Processor 등록 시 QueueName을 사용해준다.
-@Processor('Ticket')
-export class BookingProcessor {
-  constructor(private readonly bookingService: BookingService) {}
+// import { BookingService } from './booking.service';
+// import { Process, Processor } from '@nestjs/bull';
+// import { Job } from 'bull';
 
-  // 특정 Job을 동작시키기 위해 Process에 job name을 사용해준다.
-  // 'createBooking'으로 작성
-  @Process('createBooking')
-  async createBooking(job: Job<unknown>) {
-    // Job을 변수로 사용, Queue에 Job이 등록되면 Job을 가져온다.
-    const bookingData = job.data;
-    // createBooking 메서드를 통해 예매 진행
-    const createBooking = await this.bookingService.createBooking(bookingData);
-  }
-}
+// // Processor 등록 시 QueueName을 사용해준다.
+// @Processor('Ticket')
+// export class BookingProcessor {
+//   constructor(private readonly bookingService: BookingService) {}
+
+//   // 특정 Job을 동작시키기 위해 Process에 job name을 사용해준다.
+//   // 'createBooking'으로 작성
+//   @Process('createBooking')
+//   async createBooking(job: Job<unknown>) {
+//     // Job을 변수로 사용, Queue에 Job이 등록되면 Job을 가져온다.
+//     const bookingData = job.data;
+//     // createBooking 메서드를 통해 예매 진행
+//     const createBooking = await this.bookingService.createBooking(bookingData);
+//   }
+// }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -8,7 +8,7 @@ import { Redis } from 'ioredis';
 import { GoodsEntity } from 'src/database/entity/goods.entity';
 
 @Injectable()
-export class BookingService implements OnModuleInit {
+export class BookingService {
   constructor(
     @InjectRepository(BookingEntity)
     private bookingRepository: Repository<BookingEntity>,
@@ -19,15 +19,30 @@ export class BookingService implements OnModuleInit {
     this.redisClient = redisClient;
   }
   async onModuleInit() {
-    // Redis Stream을 처리하기 위한 로직을 여기에서 시작합니다.
-    this.processStream();
+    // Redis Stream을 처리하기 위한 로직을 여기에서 시작.
+    await this.processStream();
   }
 
   async processStream() {
     // 1. 스트림 및 그룹 식별자 부여
     const streamName = 'bookingStream'; // Pub에서 보내는 이름과 같게
-    const groupName = 'bookingGroup';
+    const groupName = 'bookingStream';
     const consumer = 'consumer-1';
+    let readSwitch;
+    // let streamInfo;
+    try {
+      const streamInfo = await this.redisClient.xinfo('STREAM', streamName);
+      console.log('Stream Info:', streamInfo);
+
+      if (!streamInfo) {
+        console.log('해당 스트림 데이터가 존재하지 않으므로 새로 생성');
+        await this.redisClient.xadd(streamName, '*', 'dummy', 'true');
+        // await this.redisClient.xtrim(streamName, 'MAXLEN', 0);
+        readSwitch = true;
+      }
+    } catch (err) {
+      console.error(err, '스트림 데이터 정보를 가져오지 못했음');
+    }
 
     // 2. Consumer Group 생성
     try {
@@ -39,36 +54,44 @@ export class BookingService implements OnModuleInit {
         'MKSTREAM',
       );
     } catch (err) {
-      if (
-        !err.message.includes('BUSYGROUP Consumer Group name already exists')
-      ) {
-        console.error(err);
+      {
+        console.error('컨슈머 그룹 생성 실패:', err);
       }
     }
 
     // 3.메세지를 계속 읽기
-    while (true) {
-      const entries = await this.redisClient.xreadgroup(
-        'GROUP', // 소비자 그룹에서 읽기 작업
-        groupName, // 소비자 그룹 이름
-        consumer, // 소비자 그룹내의 소비자 이름임.
-        'BLOCK', // 읽을 메시지가 더 이상 없으면 새 메시지가 도착 하거나 지정된 시간 초과가 발생할 때까지 호출이 차단(대기)됨
-        5000, //
-        'STREAMS',
-        streamName,
-        '>',
-      );
+    while (readSwitch) {
+      try {
+        const entries = await this.redisClient.xreadgroup(
+          'GROUP', // 소비자 그룹에서 읽기 작업
+          groupName, // 소비자 그룹 이름
+          consumer, // 소비자 그룹내의 소비자 이름임.
+          'BLOCK', // 읽을 메시지가 더 이상 없으면 새 메시지가 도착 하거나 지정된 시간 초과가 발생할 때까지 호출이 차단(대기)됨
+          5000, // BLOCK 5초
+          'STREAMS',
+          streamName, // 스트림 이름
+          '>', // 새로운 메세지만 보고 싶음.
+        );
 
-      if (entries) {
-        console.log(entries);
-        const streamEntires = entries[0][1];
-        console.log('streamEntires:', streamEntires);
-        for (const [id, entry] of streamEntires) {
-          console.log(id, entry);
-          const booking = JSON.parse(entry[1]);
-          console.log('booking:', booking);
-          await this.createBooking(booking);
-          await this.redisClient.xack(streamName, groupName, id);
+        if (entries) {
+          // console.log('entries: ', entries);
+          const streamEntires = entries[0][1];
+          // console.log('streamEntires:', streamEntires);
+          for (const [id, entry] of streamEntires) {
+            console.log(id, entry);
+            const booking = JSON.parse(entry[1]);
+            // console.log('booking:', booking);
+            await this.createBooking(booking);
+            await this.redisClient.xack(streamName, groupName, id);
+          }
+        }
+      } catch (err) {
+        console.error(err);
+        // break;  break 을 해버리면 서버를 재실행 해야한다.
+        if (err.message.includes('NOGROUP')) {
+          console.error('해당 스트림 혹은 스트림 그룹이 없을 경우 ');
+          readSwitch = false;
+          await this.processStream();
         }
       }
     }
